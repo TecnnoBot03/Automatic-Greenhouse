f1514f8a4b44951d2d4cc5d3d8a6cdbf7b572d11
TecnnoBot03 <simoncarreno2006@gmail.com>
2022-12-03 13:59:55 -0500

    correccion del commit anterior, comparacion de humedad

--- greenhouse.ino
+++ greenhouse.ino
@@ -1,17 +1,15 @@
-
-
-int TRIG = 6;			
-int ECO = 7;		
-int bomba = 3;	
-int humedadSuelo = A0;		
+int TRIG = 6;      
+int ECO = 7;    
+int bomba = 4;  
+int humedadSuelo = A0;    
 int DURACION;
 int DISTANCIA;
 
 void setup()
 {
-  pinMode(TRIG, OUTPUT); 	
-  pinMode(ECO, INPUT);		
-  pinMode(bomba, OUTPUT);	
+  pinMode(TRIG, OUTPUT);  
+  pinMode(ECO, INPUT);    
+  pinMode(bomba, OUTPUT); 
   Serial.begin(9600);
 
 }
@@ -25,10 +23,17 @@ void loop()
   DURACION = pulseIn(ECO, HIGH);
   DISTANCIA = DURACION / 58.2;
   int porcentajeHumedad = map(humedad, 1023, 200, 0, 100);
-  Serial.println(porcentajeHumedad + "% de humedad");
-  Serial.println(DISTANCIA + "cm");
+  Serial.print(porcentajeHumedad);
+  Serial.println("% de humedad");
+  Serial.print(DISTANCIA);
+  Serial.println("cm");
+  if (porcentajeHumedad < 60) {
+    digitalWrite(bomba, HIGH);
+    Serial.println("SI");
+  }
+  else {
+    digitalWrite(bomba, LOW);
+    Serial.println("NO");
+  }
   delay(200);
-
-
-   
 }
