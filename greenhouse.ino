498919bc5537af25e340521a3231ea410ccf78d6
TecnnoBot03 <simoncarreno2006@gmail.com>
2022-12-03 13:33:38 -0500

    lectura de los datos del sensor ultrasonico y de humedad de suelo

--- greenhouse.ino
+++ greenhouse.ino
@@ -1,1 +1,34 @@
 
+
+int TRIG = 6;			
+int ECO = 7;		
+int bomba = 3;	
+int humedadSuelo = A0;		
+int DURACION;
+int DISTANCIA;
+
+void setup()
+{
+  pinMode(TRIG, OUTPUT); 	
+  pinMode(ECO, INPUT);		
+  pinMode(bomba, OUTPUT);	
+  Serial.begin(9600);
+
+}
+
+void loop()
+{
+  int humedad = analogRead(humedadSuelo);
+  digitalWrite(TRIG, HIGH);
+  delay(1);
+  digitalWrite(TRIG, LOW);
+  DURACION = pulseIn(ECO, HIGH);
+  DISTANCIA = DURACION / 58.2;
+  Serial.println(DISTANCIA);
+  Serial.print(" ")
+  Serial.println(humedad);
+  delay(200);
+
+
+   
+}
